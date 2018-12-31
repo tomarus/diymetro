c1eef90f8ea2d5073a908cbf4538490e30e619b8
Tommy van Leeuwen <tommy@debian.chiparus.net>
2018-12-31 18:14:28 +0100

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+DIY Metro
+=========
+
+An Intellijel Metropolis / Roland System-100 model 185 / Detroit Underground DU-SEQ clone using an Arduino Nano.
+
+This is highly experimental.
+
+https://www.youtube.com/watch?v=XjalbW90R7U
---
+++ arduino/diymetro/diymetro.ino
@@ -0,0 +1,151 @@
+// sensor inputs
+#define PIN_SPEED   A0
+#define PIN_GATELEN A1
+#define PIN_4051_Z  A2
+#define PIN_MAINSW  13
+
+#define PIN_165_CLK  7
+#define PIN_165_SHLD 8
+#define PIN_165_QH   9
+
+// sensor outputs
+#define PIN_CLKOUT  3
+#define PIN_GATEOUT 2
+#define PIN_594_SER   10
+#define PIN_594_RCLK  11
+#define PIN_594_SRCLK 12
+
+// control outputs
+#define PIN_4051_S0 4
+#define PIN_4051_S1 5
+#define PIN_4051_S2 6
+
+#define HC165_PULSE_WIDTH_USEC 5
+
+// Set SERIAL_DEBUG to print debug info to serial
+#define SERIAL_DEBUG
+
+void setup() {
+#ifdef SERIAL_DEBUG
+  Serial.begin(115200);
+#endif
+  pinMode(PIN_CLKOUT,    OUTPUT);
+  pinMode(PIN_GATEOUT,   OUTPUT);
+  pinMode(PIN_4051_S0,   OUTPUT);
+  pinMode(PIN_4051_S1,   OUTPUT);
+  pinMode(PIN_4051_S2,   OUTPUT);
+  pinMode(PIN_594_SER,   OUTPUT);
+  pinMode(PIN_594_RCLK,  OUTPUT);
+  pinMode(PIN_594_SRCLK, OUTPUT);
+  pinMode(PIN_165_CLK,   OUTPUT);
+  pinMode(PIN_165_SHLD,  OUTPUT);
+  pinMode(PIN_165_QH,    INPUT);
+  pinMode(PIN_MAINSW,    INPUT);
+}
+
+int step = 0;
+int stepCount = 0;
+int maxSteps = 1;
+int reverse = 0;
+
+void loop() {
+  stepCount++; 
+  if (stepCount >= maxSteps) {
+    stepCount = 0;
+
+    int pingpong = digitalRead(PIN_MAINSW);
+    if (reverse && pingpong) {
+      step--;
+      if (step < 0) {
+        step = 7;
+        if (pingpong) { reverse = 0; step = 1; }
+      }
+    } else {
+      step++; 
+      if (step == 8) {
+        step = 0;
+        if (pingpong) { reverse = 1; step = 6; }
+      }
+    }
+  }
+
+  digitalWrite(PIN_4051_S0, bitRead(step, 0));
+  digitalWrite(PIN_4051_S1, bitRead(step, 1));
+  digitalWrite(PIN_4051_S2, bitRead(step, 2));
+  delay(2); // FIXME
+  maxSteps = analogRead(PIN_4051_Z) / 128;
+
+  unsigned char switches = read_shift_reg();
+  float gatelen = float(analogRead(PIN_GATELEN)) / 1024.0;
+  
+#ifdef SERIAL_DEBUG
+  Serial.print("step: ");      Serial.print(step);
+  Serial.print(" maxsteps: "); Serial.print(maxSteps);
+  Serial.print(" speed: ");    Serial.print(analogRead(PIN_SPEED));
+  Serial.print(" gatelen: ");  Serial.print(gatelen);
+  Serial.print(" mainsw: ");   Serial.print(digitalRead(PIN_MAINSW));
+  Serial.print(" switches: "); Serial.print(switches, BIN);
+  Serial.println("");
+#endif
+
+  float sleep = 60000.0 / (float(analogRead(PIN_SPEED)) + 10.0);
+  bool sw = switches & (1 << step);
+  digitalWrite(PIN_CLKOUT, HIGH);
+  if (sw) {
+    digitalWrite(PIN_GATEOUT, HIGH);
+    shiftOut(1 << step);
+  }
+  delayMicroseconds(10);
+  digitalWrite(PIN_CLKOUT, LOW);
+  delay(sleep * gatelen);
+
+  if (sw) {
+    digitalWrite(PIN_GATEOUT, LOW);
+    shiftOut(0);
+  }
+  delay(sleep * (1 - gatelen));
+}
+
+// read_shift_reg reads the 74hc165 shift register for all switch values.
+unsigned char read_shift_reg() {
+  unsigned char byte = 0;
+
+  // Trigger parallel load first
+  digitalWrite(PIN_165_SHLD, LOW);
+  delayMicroseconds(HC165_PULSE_WIDTH_USEC);
+  digitalWrite(PIN_165_SHLD, HIGH);
+  delayMicroseconds(HC165_PULSE_WIDTH_USEC);
+
+  for (int i = 0; i < 8; i++) {
+    int bit = digitalRead(PIN_165_QH);
+    byte |= (bit << (7 - i));
+
+    digitalWrite(PIN_165_CLK, HIGH);
+    delayMicroseconds(HC165_PULSE_WIDTH_USEC);
+    digitalWrite(PIN_165_CLK, LOW);
+    delayMicroseconds(HC165_PULSE_WIDTH_USEC);
+  }
+  return(byte);
+}
+
+// shiftOut writes a byte to the hc594 shift register
+// used to control the LEDs.
+void shiftOut(byte data) {
+  digitalWrite(PIN_594_RCLK, LOW);
+  for (int i = 7; i >= 0; i--)  {
+    digitalWrite(PIN_594_SRCLK, LOW);
+
+    int pinState;
+    if ( data & (1 << i) ) {
+      pinState = 1;
+    } else {
+      pinState = 0;
+    }
+
+    digitalWrite(PIN_594_SER, pinState);
+    digitalWrite(PIN_594_SRCLK, HIGH);
+    digitalWrite(PIN_594_SER, LOW);
+  }
+  digitalWrite(PIN_594_SRCLK, LOW);
+  digitalWrite(PIN_594_RCLK, HIGH);
+}
