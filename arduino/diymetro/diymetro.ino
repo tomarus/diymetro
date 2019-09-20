715d83f9721b569255aca3c6ed783c145982c5c5
Tommy van Leeuwen <tommy@debian.chiparus.net>
2019-09-20 18:05:54 +0200

    Update code for 1.0 boards.

--- arduino/diymetro/diymetro.ino
+++ arduino/diymetro/diymetro.ino
@@ -1,51 +1,35 @@
 // Timing configurables, all values are in microseconds.
 // You might need to tweak these depending on your hardware.
-#define CLOCK_LENGTH 50
-#define GATE_DELAY 1400
+#define CLOCK_LENGTH 250
+#define GATE_DELAY 1000
 
 // Defines the max length in microsecs of the gate length pot.
 #define MAX_GATE_TIME 1000000
 
-// Configure the main switch behaviour.
-// Mode 0 = Switch between forward & pingpong mode.
-// Mode 1 = Switch between substep & pingpong mode.
-// Mode 2 = Switch between substep & forward mode.
-#define MAINSW_MODE 0
-
-// "Substep" is inspired by the System 100m 185 mk2 Split Mode.
-// https://www.youtube.com/watch?v=KMH-QNW8toE
-// Set to 16 to play 3 x notes 1-4 and 1 x notes 5-8
-// Set to 32 to play 7 x notes 1-4 and 1 x notes 5-8
-// (And 8 would be the same as forward mode)
-#define SUBSTEP_PATTERN_LENGTH 24
-
 // sensor inputs
-#define PIN_SPEED A0
-#define PIN_GATELEN A1
+#define PIN_SPEED A1
+#define PIN_GATELEN A0
 #define PIN_4051_Z A2
 #define PIN_MAINSW A3
 #define PIN_CLKINSW A4
+#define PIN_GATESW A5
 #define PIN_CLKIN 2
 #define PIN_RSTIN 3
 
-#define PIN_165_CLK 13
-#define PIN_165_SHLD 8
-#define PIN_165_QH 9
+#define PIN_DEBUG_LED 9
 
 // sensor outputs
-#define PIN_CLKOUT 7
-#define PIN_GATEOUT 12
-#define PIN_594_SER 10
-#define PIN_594_RCLK 11
-#define PIN_594_SRCLK 13
+#define PIN_CLKOUT 7	 // PD7
+#define PIN_GATEOUT 12   // PB4
+#define PIN_594_SER 10   // PB2
+#define PIN_594_RCLK 11  // PB3
+#define PIN_594_SRCLK 13 // PB5
 
 // 4051 port configuration.
 // You can't change this without changing PORTD logic below.
 #define PIN_4051_S0 4
 #define PIN_4051_S1 5
 #define PIN_4051_S2 6
-
-#define HC165_PULSE_WIDTH_USEC 5
 
 void setup()
 {
@@ -57,12 +41,11 @@ void setup()
 	pinMode(PIN_594_SER, OUTPUT);
 	pinMode(PIN_594_RCLK, OUTPUT);
 	pinMode(PIN_594_SRCLK, OUTPUT);
-	pinMode(PIN_165_CLK, OUTPUT);
-	pinMode(PIN_165_SHLD, OUTPUT);
-	pinMode(PIN_165_QH, INPUT);
+	pinMode(PIN_DEBUG_LED, OUTPUT);
 	pinMode(PIN_CLKIN, INPUT_PULLUP);
 	attachInterrupt(digitalPinToInterrupt(PIN_CLKIN), clock_receive, RISING);
 	attachInterrupt(digitalPinToInterrupt(PIN_RSTIN), reset_receive, RISING);
+	randomSeed(analogRead(0));
 }
 
 int step = 0;
@@ -95,6 +78,9 @@ void reset_receive()
 int myAnalogRead(int pin)
 {
 	analogRead(pin);
+	delayMicroseconds(500);
+	analogRead(pin);
+	delayMicroseconds(500);
 	return analogRead(pin);
 }
 
@@ -121,16 +107,19 @@ void loop()
 	float gatelen = float(myAnalogRead(PIN_GATELEN)) / 1024.0;
 
 	// calc note length in microseconds, this ranges from about 6 to 300 bpm or so
-	unsigned long speed = 60000000.0 / (float(myAnalogRead(PIN_SPEED)) + 10.0);
+	unsigned long speed = 45000000.0 / (float(myAnalogRead(PIN_SPEED)) + 10.0);
 
 	// clocksw is high if an external clock is plugged in.
-	bool intclock = myAnalogRead(PIN_CLKINSW) < 512;
+	//	bool intclock = myAnalogRead(PIN_CLKINSW) < 512;
+	int ic = myAnalogRead(PIN_CLKINSW);
+	bool intclock = ic > 10 && ic < 900;
+	digitalWrite(PIN_DEBUG_LED, intclock);
 
 	// if the gate is still open, turn it off after some time.
 	if (gateOpen && lastNotePlayed < micros() - (MAX_GATE_TIME * gatelen))
 	{
 		digitalWrite(PIN_GATEOUT, LOW);
-		shiftOut(0);
+		shiftOut2(0);
 		gateOpen = false;
 	}
 
@@ -157,7 +146,7 @@ void loop()
 	if (gateOpen)
 	{
 		digitalWrite(PIN_GATEOUT, LOW);
-		shiftOut(0);
+		shiftOut2(0);
 		gateOpen = false;
 	}
 
@@ -169,18 +158,30 @@ void loop()
 // necessary advances the sequencer by a step.
 bool do_step()
 {
+	bool advanced = false;
 	stepCount++;
 	if (stepCount >= maxSteps)
 	{
 		stepCount = 0;
 		advance();
+		advanced = true;
 	}
 
 	// Set PIN_4051_Sxx
 	PORTD = (PORTD & 0b10001111) | (step << 4);
 
-	unsigned char switches = read_shift_reg();
-	bool sw = switches & (1 << step);
+	// Check the value of the step switch.
+	// It's either 0, 511 or 1023 (with some margin).
+	int gatesw = myAnalogRead(PIN_GATESW);
+	int sw = 0; // default off (middle position)
+	if (gatesw > 400 && gatesw < 600)
+	{
+		sw = 1; // down
+	}
+	if (gatesw > 900)
+	{
+		sw = 2; // up
+	}
 
 	// Wait for this many microseconds after switching the 4051
 	// before sending the gate signal.
@@ -188,26 +189,27 @@ bool do_step()
 
 	// toggle clock and gate if the gate switch is on.
 	digitalWrite(PIN_CLKOUT, HIGH);
-	if (sw)
+	bool gate = sw == 1 || (sw == 2 && advanced);
+	if (gate)
 	{
 		digitalWrite(PIN_GATEOUT, HIGH);
-		shiftOut(1 << step);
+		shiftOut2(1 << step);
 	}
 	// clock time is 10us
 	delayMicroseconds(CLOCK_LENGTH);
 	digitalWrite(PIN_CLKOUT, LOW);
 
-	maxSteps = map(myAnalogRead(PIN_4051_Z), 0, 1023, 1, 8); // / 128;
+	maxSteps = map(myAnalogRead(PIN_4051_Z), 0, 1023, 8, 1); // / 128;
 
 	// return whether or not the gate was triggered.
-	return sw;
+	return gate;
 }
 
 // advance the sequencer in forward mode
-void advance_fwd()
+void advance_fwd(int max)
 {
 	step++;
-	if (step == 8)
+	if (step >= max)
 	{
 		step = 0;
 	}
@@ -229,7 +231,7 @@ void advance_pingpong()
 	else
 	{
 		step++;
-		if (step == 8)
+		if (step >= 8)
 		{
 			step = 0;
 			reverse = 1;
@@ -239,15 +241,15 @@ void advance_pingpong()
 }
 
 // advance the sequencer in substep mode
-void advance_substep()
+void advance_substep(int len)
 {
 	substep++;
-	if (substep == SUBSTEP_PATTERN_LENGTH)
+	if (substep >= len)
 	{
 		substep = 0;
 	}
 
-	if (substep < SUBSTEP_PATTERN_LENGTH - 4)
+	if (substep < len - 4)
 	{
 		// play notes 1-4
 		step = substep % 4;
@@ -255,100 +257,99 @@ void advance_substep()
 	else
 	{
 		// play notes 5-8
-		step = substep + 8 - SUBSTEP_PATTERN_LENGTH;
+		step = substep + 8 - len;
 	}
 }
 
+void advance_random()
+{
+	step = random(8);
+}
+
+byte rndsteps[8];
+int rndstep;
+void advance_random_preset_init()
+{
+	step = random(8);
+	rndstep = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		rndsteps[i] = i;
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		int x = random(7) + 1;
+		byte t = rndsteps[x];
+		rndsteps[x] = rndsteps[i];
+		rndsteps[i] = t;
+	}
+}
+void advance_random_preset()
+{
+	rndstep++;
+	if (rndstep > 8)
+	{
+		rndstep = 0;
+	}
+	step = rndsteps[rndstep];
+}
+
+int lastmainsw = 0;
 // advance advances the sequencer by one step
 void advance()
 {
-	bool mainsw = myAnalogRead(PIN_MAINSW) < 768;
+	int mainsw = map(myAnalogRead(PIN_MAINSW), 0, 1023 - (1023 / 12), 11, 0);
+	bool changed = false;
 
-	// I might redo this in the future. Instead of having a switch and
-	// one #define statement, I might add a pushbutton and a LED indicator
-	// of the mode selected. A random() mode would be nice to have too.
-
-#if MAINSW_MODE == 0
-	if (mainsw)
+	if (lastmainsw != mainsw)
 	{
-		advance_fwd();
+		lastmainsw = mainsw;
+		//    step = 0;
+		//    substep = 0;
+		changed = true;
 	}
-	else
+
+	switch (mainsw)
 	{
+	case 0:
+		advance_fwd(4);
+		break;
+	case 1:
+		advance_fwd(6);
+		break;
+	case 2:
+		advance_fwd(8);
+		break;
+	case 3:
 		advance_pingpong();
+		break;
+	case 4:
+		advance_substep(16);
+		break;
+	case 5:
+		advance_substep(24);
+		break;
+	case 6:
+		advance_substep(32);
+		break;
+	case 7:
+		advance_random();
+		break;
+	default:
+		if (changed)
+		{
+			advance_random_preset_init();
+		}
+		advance_random_preset();
+		break;
 	}
-
-#elif MAINSW_MODE == 1
-	if (mainsw)
-	{
-		advance_substep();
-	}
-	else
-	{
-		advance_pingpong();
-	}
-
-#elif MAINSW_MODE == 2
-	if (mainsw)
-	{
-		advance_fwd();
-	}
-	else
-	{
-		advance_substep();
-	}
-#else
-#error "Wrong MAINSW_MODE defined"
-#endif
-}
-
-// read_shift_reg reads the 74hc165 shift register for all switch values.
-unsigned char read_shift_reg()
-{
-	unsigned char byte = 0;
-
-	// Trigger parallel load first
-	digitalWrite(PIN_165_SHLD, LOW);
-	delayMicroseconds(HC165_PULSE_WIDTH_USEC);
-	digitalWrite(PIN_165_SHLD, HIGH);
-	delayMicroseconds(HC165_PULSE_WIDTH_USEC);
-
-	for (int i = 0; i < 8; i++)
-	{
-		int bit = digitalRead(PIN_165_QH);
-		byte |= (bit << (7 - i));
-
-		digitalWrite(PIN_165_CLK, HIGH);
-		delayMicroseconds(HC165_PULSE_WIDTH_USEC);
-		digitalWrite(PIN_165_CLK, LOW);
-		delayMicroseconds(HC165_PULSE_WIDTH_USEC);
-	}
-	return (byte);
 }
 
 // shiftOut writes a byte to the hc594 shift register
 // used to control the LEDs.
-void shiftOut(byte data)
+void shiftOut2(byte data)
 {
 	digitalWrite(PIN_594_RCLK, LOW);
-	for (int i = 7; i >= 0; i--)
-	{
-		digitalWrite(PIN_594_SRCLK, LOW);
-
-		int pinState;
-		if (data & (1 << i))
-		{
-			pinState = 1;
-		}
-		else
-		{
-			pinState = 0;
-		}
-
-		digitalWrite(PIN_594_SER, pinState);
-		digitalWrite(PIN_594_SRCLK, HIGH);
-		digitalWrite(PIN_594_SER, LOW);
-	}
-	digitalWrite(PIN_594_SRCLK, LOW);
+	shiftOut(PIN_594_SER, PIN_594_SRCLK, MSBFIRST, data);
 	digitalWrite(PIN_594_RCLK, HIGH);
 }
